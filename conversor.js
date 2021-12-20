074a3ec8dff692c6154d6fccdec17f450c19db18
wesley <wesleyramenhol@hotmail.com>
2021-12-19 22:10:46 -0300

    adicionando

---
+++ conversor.js
@@ -0,0 +1,15 @@
+document.getElementById('convert').onclick = tempconvert;
+document.getElementById('clear').onclick = clearform;
+
+function tempconvert(){
+    var fahrenheit = document.getElementById("fahrenheit").Value;
+    var celsius = document.getElementById("celsius").Value;
+
+    if(isNaN(fahrenheit) || isNaN(celsius)){
+        alert("digite um valor valido");
+        return
+    }else if{
+        
+    }
+
+}
