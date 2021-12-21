f9f1da8d179d4904ed3e7dd0375a2d4e745db750
wesley <wesleyramenhol@hotmail.com>
2021-12-20 22:30:42 -0300

    temperatura

--- conversor.js
+++ conversor.js
@@ -1,15 +1,31 @@
-document.getElementById('convert').onclick = tempconvert;
-document.getElementById('clear').onclick = clearform;
 
-function tempconvert(){
-    var fahrenheit = document.getElementById("fahrenheit").Value;
-    var celsius = document.getElementById("celsius").Value;
 
-    if(isNaN(fahrenheit) || isNaN(celsius)){
-        alert("digite um valor valido");
+function tempconvert() {
+    var fahrenheit = document.getElementById("fahrenheit").value;
+    var celsius = document.getElementById("celsius").value;
+
+    if (isNaN(fahrenheit) || isNaN(celsius)) {
+        alert("digite um valor válido!");
         return
-    }else if{
-        
+    } else if (fahrenheit === '') {
+        fahrenheit = (parseFloat(celsius) * 1.8) + 32;
+    } else if (celsius === '') {
+        celsius = (parseFloat(fahrenheit) - 32) / 1.8;
+    } else {
+        alert("obrigátorio um campo vazio!");
+        zerar()
+        return
     }
+
+    document.getElementById("fahrenheit").value = parseFloat(fahrenheit).toFixed(1);
+    document.getElementById("celsius").value = parseFloat(celsius).toFixed(1);
+
+
+}
+function zerar(){
+    
+    document.getElementById("fahrenheit").value = '';
+    document.getElementById("celsius").value = '';
+
 
 }
